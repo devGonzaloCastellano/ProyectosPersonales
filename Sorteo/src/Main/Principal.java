b55c34aad848724557b89f2967e551e80ea2405a
unknown <monocastellano@outlook.com>
2024-06-16 10:14:41 -0300

    Check

---
+++ Sorteo/src/Main/Principal.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Vista.Inicio;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Inicio ini = new Inicio();
+        ini.setTitle("Sorteo");
+        ini.setResizable(false);
+        ini.setLocationRelativeTo(null);
+        ini.setVisible(true);
+
+    }
+}
